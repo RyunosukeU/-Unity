9e7fe7674d5291f5d72aa8195f7f9283a2ea452a
Haruki Oshiro <e205721@ie.u-ryukyu.ac.jp>
2021-12-09 10:44:01 +0900

    add bullet move script

---
+++ シューティング/Assets/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+Bulletにこのスクリプトをくっつけてプレハブ化。
+
+SerializeFieldつけてるのでUnityEditorのInspectorで弾のスピードいじれます。
+
+y座標が５より大きくなったら自分自身を破壊するようにしてます。
+*/
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 0.1f;
+
+    void Update()
+    {
+        move();
+        destroyCheck();
+    }
+
+    void move(){
+        transform.Translate(0,bulletSpeed,0);
+    }
+    void destroyCheck(){
+        if (transform.position.y > 5){
+            Destroy(gameObject);
+        }
+    }
+}
